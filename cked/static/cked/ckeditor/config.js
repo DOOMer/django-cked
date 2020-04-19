9288832c629f02d05784d870fadaeb6d89b959c9
DOOMer <doomer3d@gmail.com>
2020-04-19 19:11:32 +0300

    Update CKeditor to 4.14.0

--- cked/static/cked/ckeditor/config.js
+++ cked/static/cked/ckeditor/config.js
@@ -1,10 +1,9 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	config.filebrowserBrowseUrl = '';
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
