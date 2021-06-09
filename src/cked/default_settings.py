48a9c2703a3433f6d634217a562c032a6edbbdb5
DOOMer <doomer3d@gmail.com>
2021-06-09 17:28:05 +0300

    Migrate from setuptools to poetry and move 'cked' to 'src/cked'

---
+++ src/cked/default_settings.py
@@ -0,0 +1,11 @@
+
+ELFINDER_DEFAULT_OPTIONS = {
+}
+
+CKEDITOR_DEFAULT_OPTIONS = {
+    'skin': 'moono-lisa',
+    'preset': 'full',
+    'filebrowserWindowWidth': 940,
+    'filebrowserWindowHeight': 450,
+    'language': 'ru',
+}
